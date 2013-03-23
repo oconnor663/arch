e62868754ce11f5af409b1f6b5608a53f01fafc5
Jack O'Connor <jacko@fb.com>
2013-03-23 11:42:19 -0700

    more pave.sh

--- pave.sh
+++ pave.sh
@@ -2,16 +2,32 @@
 
 set -ex
 
-disk=/dev/sda
+chroot="arch-chroot /mnt"
 
 # clear the partition table and create one big partition
-sgdisk -o $disk
-sgdisk -n 0:0:0 $disk
-mkfs -t ext4 ${disk}1
-mount ${disk}1 /mnt
+sgdisk -o /dev/sda
+sgdisk -n 0:0:0 /dev/sda
+mkfs -t ext4 /dev/sda1
+mount /dev/sda1 /mnt
 
-pacstrap /mnt base base-devel
-
-arch-chroot /mnt pacman -S --noconfirm syslinux
+# install base packages. syslinux is our bootloader, and it requires
+# gptfdisk to work with the GPT partitions we created above
+pacstrap /mnt base base-devel syslinux gptfdisk
 
 genfstab -p /mnt >> /mnt/etc/fstab
+
+echo arch-host > /mnt/etc/hostname
+
+ln -s /usr/share/zoneinfo/US/Pacific /mnt/etc/localtime
+
+echo 'LANG="en_US.UTF-8"' > /mnt/etc/locale.conf
+echo 'en_US.UTF-8 UTF-8'  > /mnt/etc/locale.gen
+$chroot locale-gen
+
+$chroot mkinitcpio -p linux
+
+$chroot syslinux-install_update -i -a -m
+# syslinux targets sda3 by default, not sure why
+sed -i s/sda3/sda1/g /mnt/boot/syslinux/syslinux.cfg
+
+$chroot passwd
