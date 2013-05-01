74db4d770c18a6afc96f9bec298811b2c21cd5df
Jack O'Connor <oconnor663@gmail.com>
2013-05-01 14:56:49 -0700

    Remove nox2apic kernel parameter
    It turns out that this boot issue is an intersection of three things:
    1) Nvidia
    2) X2APIC
    3) VT-d
    I had enabled #3 as part of setting up virtualbox. Simply disabling that
    again makes the laptop boot just fine.
    
    See:
    https://fedoraproject.org/wiki/Common_F19_bugs#Boot_hangs_when_using_NVIDIA_discrete_graphics_on_some_Thinkpad_models_.28W520.2C_T420.29

--- setup-t420.sh
+++ setup-t420.sh
@@ -2,11 +2,6 @@
 
 set -ev
 
-# Linux won't boot properly on this laptop without the 'nox2apic' kernel
-# parameter. Something like this is required for Ubuntu too.
-sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="nox2apic"/' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
-
 # We need the proprietary Nvidia drivers to handle our external monitors.
 if pacman -Qsq | grep mesa-libgl > /dev/null; then
   # This package conflicts with nvidia-libgl. Force remove it if it's installed.
