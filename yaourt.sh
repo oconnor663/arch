d261ca72cb1f106b8a5b1068ed2bea8266afc539
Jack O'Connor <oconnor663@gmail.com>
2014-10-19 16:44:29 -0700

    yaourt.sh

---
+++ yaourt.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+echo Installing package-query...
+cd $(mktemp -d)
+curl https://aur.archlinux.org/packages/pa/package-query/PKGBUILD > PKGBUILD
+makepkg --asroot --syncdeps --install --noconfirm
+
+echo Installing yaourt...
+cd $(mktemp -d)
+curl https://aur.archlinux.org/packages/ya/yaourt/PKGBUILD > PKGBUILD
+makepkg --asroot --syncdeps --install --noconfirm
