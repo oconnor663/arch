9c57cd02370da5daaaa40b82214167fb04a46bd0
Jack O'Connor <oconnor663@gmail.com>
2014-11-15 06:51:42 +0000

    add --target=i386-pc to grub-install, to force BIOS mode

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ echo 'LANG="en_US.UTF-8"' > /mnt/etc/locale.conf
 echo 'en_US.UTF-8 UTF-8'  > /mnt/etc/locale.gen
 arch-chroot /mnt locale-gen
 
-arch-chroot /mnt grub-install --recheck "$drive"
+arch-chroot /mnt grub-install --target=i386-pc --recheck "$drive"
 arch-chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
 
 arch-chroot /mnt systemctl enable NetworkManager ntpd sshd
