8c70aefde2079030d1d7d63776350467e727e765
Jack O'Connor <oconnor663@gmail.com>
2015-04-09 00:14:42 -0400

    --asroot has been removed from makepkg

--- yaourt.sh
+++ yaourt.sh
@@ -5,9 +5,9 @@ set -e
 echo Installing package-query...
 cd $(mktemp -d)
 curl https://aur.archlinux.org/packages/pa/package-query/PKGBUILD > PKGBUILD
-makepkg --asroot --syncdeps --install --noconfirm
+makepkg --syncdeps --install --noconfirm
 
 echo Installing yaourt...
 cd $(mktemp -d)
 curl https://aur.archlinux.org/packages/ya/yaourt/PKGBUILD > PKGBUILD
-makepkg --asroot --syncdeps --install --noconfirm
+makepkg --syncdeps --install --noconfirm
