f05cea0b207f7cd9df77131ef9f65f4155ab3db4
Jack O'Connor <oconnor663@gmail.com>
2016-02-15 11:10:55 -0500

    move nofiles.sh to limits.sh, and bump DefaultTasksMax

--- limits.sh
+++ limits.sh
@@ -1,14 +1,16 @@
 #! /bin/bash
 
 # The default open files limit on Linux/systemd is pretty low, 1024 (soft) and
-# 4096 (hard). Set it to a large value.
+# 4096 (hard). So is systemd's default TasksMax limit. Raise both of them.
 
 set -e -u -o pipefail
 
 mkdir -p /etc/systemd/system.conf.d
 
-cat > /etc/systemd/system.conf.d/nofile.conf << END
+cat > /etc/systemd/system.conf.d/limits.conf << END
 [Manager]
 # Allow the system to open more than a few thousand files at a time.
 DefaultLimitNOFILE=65536
+# Allow more than 512 threads.
+DefaultTasksMax=65536
 END
