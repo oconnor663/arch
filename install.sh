92f1b794d6ce43cf4911a4d3fb535334951e3bca
Jack O'Connor <oconnor663@gmail.com>
2016-06-20 11:27:46 -0400

    New_York timezone

--- install.sh
+++ install.sh
@@ -25,7 +25,7 @@ genfstab -p /mnt > /mnt/etc/fstab
 
 echo arch-host > /mnt/etc/hostname
 
-ln -sf /usr/share/zoneinfo/America/Los_Angeles /mnt/etc/localtime
+ln -sf /usr/share/zoneinfo/America/New_York /mnt/etc/localtime
 
 echo 'LANG="en_US.UTF-8"' > /mnt/etc/locale.conf
 echo 'en_US.UTF-8 UTF-8'  > /mnt/etc/locale.gen
