3ff9d9fd37e1cce34064606cffe3723383c0104a
Jack O'Connor <oconnor663@gmail.com>
2021-08-19 13:33:58 -0400

    sudo check in reflector

--- reflector.sh
+++ reflector.sh
@@ -2,5 +2,10 @@
 
 set -e -u -o pipefail
 
+if [[ "$(whoami)" != "root" ]] ; then
+    echo needs sudo
+    exit 1
+fi
+
 # Note that "US," means both US and worldwide mirros.
 reflector --country=US, --protocol=https --threads=4 --fastest=5 --save=/etc/pacman.d/mirrorlist
